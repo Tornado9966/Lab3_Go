9625d5ec87c4bb87c5198c9b1d5c9d99edfe0ee7
Iluysha <ilua.mix12@gmail.com>
2020-05-09 19:25:57 +0300

    Fix balancer

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -33,26 +33,53 @@ func (s * TestSuite) TestBalancer(c *C){
 	}
 	
 	//Tests for the chooseServer function
-	healthyServersPool = []int{0, 1, 2}
-	
+	//client's RemoteAddr 
 	addr := []string{"172.20.0.3:45234", 
                          "172.20.0.3:45234",
                          "172.20.0.4:34563",
                          "172.20.0.2:34563",
-                         "172.20.0.4:34563"}
+                         "172.20.0.4:34563",
+			 "172.20.0.3:45234",
+			 "172.20.0.4:34563",
+			 "172.20.0.2:34563"}
+	
+	//all servers are healthy
+	healthyServersPool = []int{0, 1, 2}
 	
 	for i := 0; i < len(addr); i++ {
 		res := chooseServer(addr[i]) 
-		if i == 0 || i == 1 {
+		if i == 0 || i == 1 || i == 5 {
 			c.Assert(res, Equals, "server2:8080")
 		}
-		if i == 2 || i == 4 {
+		if i == 2 || i == 4 || i == 6 {
 			c.Assert(res, Equals, "server3:8080")
 		}
-		if i == 3 {
+		if i == 3 || i == 7 {
 			c.Assert(res, Equals, "server1:8080") 
 		}
 	}
+	
+	//server2 is down
+	healthyServersPool = []int{0, 2}
+	
+	for i := 0; i < len(addr); i++ {
+		res := chooseServer(addr[i]) 
+		if i == 0 || i == 1 || i == 3 || i == 5 || i == 7{
+			c.Assert(res, Equals, "server1:8080")
+		}
+		if i == 2 || i == 4 || i == 6 {
+			c.Assert(res, Equals, "server3:8080")
+		}
+	}
+	
+	//server1 and server2 are down
+	healthyServersPool = []int{2}
+
+	for i := 0; i < len(addr); i++ {
+		res := chooseServer(addr[i]) 
+		c.Assert(res, Equals, "server3:8080")
+	}
+	
 	
 	//Tests for the remove function
 	arr := []int{1, 2, 3}
